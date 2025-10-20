1e709c732bbd21001f6a2449a1581ef4fdd97a44
Matej <matej.galic.ets@gmail.com>
2025-10-20 17:29:12 +0200

    init

---
+++ src/main/java/hr/algebra/uni_course_management/UniCourseManagementApplication.java
@@ -0,0 +1,11 @@
+package hr.algebra.uni_course_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniCourseManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UniCourseManagementApplication.class, args);
+    }
+}
