98cd96b97668e4acaae932e74a1f01a737f039b6
Matej <matej.galic.ets@gmail.com>
2025-12-24 15:18:41 +0100

    added first and last name in register.html and message after registering new user

--- src/main/java/hr/algebra/uni_course_management/controller/UserController.java
+++ src/main/java/hr/algebra/uni_course_management/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 
@@ -54,10 +55,12 @@ public class UserController {
             @RequestParam String firstName,
             @RequestParam String lastName,
             @RequestParam String role,
+            RedirectAttributes redirectAttributes,
             Model model) {
         try {
             userService.registerUser(username, password, firstName, lastName, UserRole.valueOf(role.toUpperCase()));
-            return "redirect:/login";
+            redirectAttributes.addFlashAttribute("successMessage", "User '" + username + "' created successfully!");
+            return "redirect:/dashboard";
         } catch (IllegalArgumentException e) {
             model.addAttribute("errorMessage", e.getMessage());
             return "redirect:/register?error=" + e.getMessage();
